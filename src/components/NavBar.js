6e45c5ddf3b37edd5b4523ea7d79560c0b1ba2cd
SantGT5 <gianspf@gmail.com>
2021-09-04 21:47:38 +0200

    GianLucas - CV && NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import myCV from "./MyCV/GianLucas - CV.pdf";
@@ -8,9 +7,11 @@ function NavBar() {
   return (
     <div className="navBarDes fixed-top">
       <div className="navbar">
-        <div style={{ background: "#181818" }} className="container-fluid">
-          <Link
-            to="/"
+        <div
+          style={{ background: "#181818", flexWrap: "nowrap" }}
+          className="container-fluid"
+        >
+          <div
             style={{ color: "white", fontSize: "25px" }}
             className="navbar-brand"
           >
@@ -21,58 +22,67 @@ function NavBar() {
             <div className="err" title="404">
               404
             </div>
-          </Link>
+          </div>
 
           <div className="navIcon">
             <a href="mailto:gianspf@gmail.com?subject=Let's Talk!">
               <i style={{ fontSize: "30px" }} className="far fa-envelope"></i>
             </a>
 
-            <a rel="noreferrer" href="https://github.com/SantGT5" target="_blank">
+            <a
+              rel="noreferrer"
+              href="https://github.com/SantGT5"
+              target="_blank"
+            >
               <i style={{ fontSize: "30px" }} className="fab fa-github"></i>
             </a>
 
-            <a rel="noreferrer" href="https://www.linkedin.com/in/gian-lucas/?locale=en_US" target="_blank">
+            <a
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/gian-lucas/?locale=en_US"
+              target="_blank"
+            >
               <i
                 style={{ fontSize: "30px" }}
                 className="fab fa-linkedin-in"
               ></i>
             </a>
 
-            <a rel="noreferrer" href="https://www.instagram.com/giaanlucas_/" target="_blank">
+            <a
+              rel="noreferrer"
+              href="https://www.instagram.com/giaanlucas_/"
+              target="_blank"
+            >
               <i style={{ fontSize: "30px" }} className="fab fa-instagram"></i>
             </a>
-            
           </div>
 
-
-            {/* <Link to="/" style={{ color: "white" }} className="navbar-brand">
+          {/* <Link to="/" style={{ color: "white" }} className="navbar-brand">
             <i className="fas fa-file-download"></i> Curriculum
             </Link> */}
 
-<DropdownButton
-              id="dropdown-button-dark-example2"
-              variant="#181818"
-              menuVariant="dark"
-              title="My CV"
-              className="drop"
+          <DropdownButton
+            id="dropdown-button-dark-example2"
+            variant="#181818"
+            menuVariant="dark"
+            title="My CV"
+            className="drop"
+          >
+            <Dropdown.Item
+              style={{ color: "white" }}
+              href={myCV}
+              download="GianLucas - CV"
             >
-              <Dropdown.Item
-                style={{ color: "white" }}
-                href={myCV}
-                download="GianLucas - CV"
-              >
-                PDF Version
-              </Dropdown.Item>
-              <Dropdown.Item
-                style={{ color: "white" }}
-                href={myCVPNG}
-                download="GianLucas - CV"
-              >
-                PNG Version
-              </Dropdown.Item>
-            </DropdownButton>
-
+              PDF Version
+            </Dropdown.Item>
+            <Dropdown.Item
+              style={{ color: "white" }}
+              href={myCVPNG}
+              download="GianLucas - CV"
+            >
+              PNG Version
+            </Dropdown.Item>
+          </DropdownButton>
         </div>
       </div>
     </div>
